de0876c34221febc0e47d8db7351499e7a886386
Arjun Sarode <sarodearjun57@gmail.com>
2017-04-15 20:00:31 -0700

    pick cooler settings

--- modules/boids.js
+++ modules/boids.js
@@ -2,29 +2,34 @@ import Regl from 'regl'
 import vec3 from 'gl-vec3'
 import mat4 from 'gl-mat4'
 import hsv2rgb from 'hsv2rgb'
+import Stats from 'stats.js'
+import isEqual from 'lodash/isEqual'
+
+const stats = new Stats()
+stats.showPanel(0)
+document.body.appendChild(stats.dom)
 
 const regl = Regl()
 
-const NUM_BOIDS = 128
-const BOID_DATA_SIZE = 4 * (3 + 3 + 3)
-const boidData = Array(NUM_BOIDS).fill().map(() => {
-  const color = hsv2rgb(Math.random() * 110 + 250, 0.6, 1).map(v => v / 255)
-  const position = vec3.random([], 25)
-  const velocity = vec3.random([], 0.25)
-  return [
-    position,
-    velocity,
-    color
-  ]
-})
-const boidBuffer = regl.buffer({
-  data: boidData
-})
+const options = {
+  numBoids: 500,
+  nearbyDistance: 8,
+  avoidDistance: 4,
+  followMultiplier: 0.2,
+  toCenterMultiplier: 0.02,
+  maxSpeed: 6,
+  minSpeed: 0.2,
+  boundSize: 30,
+  boundCorrection: 0.3,
+  stepSize: 0.1
+}
+
+var BOID_DATA
+var BOID_BUFFER
 
 const getNearbyBoids = (boid, allBoids) => {
   return allBoids.filter((b) => {
-    // return b !== boid
-    return b !== boid && vec3.squaredDistance(boid[0], b[0]) < 100
+    return b !== boid && vec3.squaredDistance(boid[0], b[0]) < Math.pow(options.nearbyDistance, 2)
   })
 }
 
@@ -36,14 +41,14 @@ const flyToCenter = (boid, nearbyBoids) => {
   }
   const center = vec3.scale([], positionSum, 1 / nearbyBoids.length)
   const vecToCenter = vec3.subtract([], center, boid[0])
-  return vec3.scale([], vecToCenter, 1 / 100)
+  return vec3.scale([], vecToCenter, options.toCenterMultiplier)
 }
 
 const flyAwayFromOthers = (boid, nearbyBoids) => {
   const vecAway = vec3.create()
   for (let i = 0; i < nearbyBoids.length; i++) {
     const b = nearbyBoids[i]
-    if (vec3.squaredDistance(b[0], boid[0]) < 25) {
+    if (vec3.squaredDistance(b[0], boid[0]) < Math.pow(options.avoidDistance, 2)) {
       const vecAwayFromBoid = vec3.subtract([], b[0], boid[0])
       vec3.subtract(vecAway, vecAway, vecAwayFromBoid)
     }
@@ -59,39 +64,58 @@ const flyInSameDir = (boid, nearbyBoids) => {
   }
   const vecSameDir = vec3.scale([], velocitySum, 1 / nearbyBoids.length)
   const vecWithoutTargetBoidVelocity = vec3.subtract([], vecSameDir, boid[1])
-  return vec3.scale([], vecWithoutTargetBoidVelocity, 1 / 8)
+  return vec3.scale([], vecWithoutTargetBoidVelocity, options.followMultiplier)
 }
 
 const limitVelocity = (velocity) => {
-  const max = 0.5
-  if (vec3.length(velocity) > max) {
-    vec3.scale(velocity, vec3.normalize(velocity, velocity), max)
+  const { maxSpeed, minSpeed } = options
+  const length = vec3.length(velocity)
+  if (length > maxSpeed) {
+    vec3.scale(velocity, vec3.normalize(velocity, velocity), maxSpeed)
+  } else if (length < minSpeed) {
+    vec3.scale(velocity, vec3.normalize(velocity, velocity), minSpeed)
   }
   return velocity
 }
 
 const keepInside = ([x, y, z], velocity) => {
-  const bound = 30
+  const bound = options.boundSize
   if (x < -bound) {
-    velocity[0] = 10
+    velocity[0] += options.boundCorrection
   } else if (x > bound) {
-    velocity[0] = -10
+    velocity[0] -= options.boundCorrection
   }
   if (y < -bound) {
-    velocity[1] = 10
+    velocity[1] += options.boundCorrection
   } else if (y > bound) {
-    velocity[1] = -10
+    velocity[1] -= options.boundCorrection
   }
   if (z < -bound) {
-    velocity[2] = 10
+    velocity[2] += options.boundCorrection
   } else if (z > bound) {
-    velocity[2] = -10
+    velocity[2] -= options.boundCorrection
   }
   return velocity
 }
 
+const BOID_DATA_SIZE = 4 * (3 + 3 + 3)
+BOID_DATA = Array(options.numBoids).fill().map(() => {
+  const color = hsv2rgb(Math.random() * 110 + 250, 0.6, 1).map(v => v / 255)
+  const position = vec3.random([], 25)
+  const velocity = vec3.random([], 0.25)
+  return [
+    position,
+    velocity,
+    color
+  ]
+})
+
+BOID_BUFFER = regl.buffer({
+  data: BOID_DATA
+})
+
 const updateBoids = () => {
-  const newBoidData = boidData.map((boid, _, data) => {
+  BOID_DATA.forEach((boid, _, data) => {
     const nearbyBoids = getNearbyBoids(boid, data)
     const rule1 = flyToCenter(boid, nearbyBoids)
     const rule2 = flyAwayFromOthers(boid, nearbyBoids)
@@ -102,15 +126,14 @@ const updateBoids = () => {
       rule1[2] + rule2[2] + rule3[2],
     ]
     const newVelocity = limitVelocity(keepInside(boid[0], vec3.add([], boid[1], ruleSum)))
-    const newPosition = vec3.add([], boid[0], newVelocity)
+    const newPosition = vec3.add([], boid[0], vec3.scale([], newVelocity, options.stepSize))
 
     boid[0] = newPosition
     boid[1] = newVelocity
-    return boid
   })
 
-  boidBuffer({
-    data: newBoidData
+  BOID_BUFFER({
+    data: BOID_DATA
   })
 }
 
@@ -122,7 +145,7 @@ const drawBoids = regl({
     varying vec3 fragColor;
 
     void main() {
-      gl_PointSize = 5.0;
+      gl_PointSize = 2.0;
       gl_Position = projection * view * vec4(position, 1);
       fragColor = color;
     }
@@ -137,24 +160,24 @@ const drawBoids = regl({
   `,
   attributes: {
     position: {
-      buffer: boidBuffer,
+      buffer: BOID_BUFFER,
       stride: BOID_DATA_SIZE,
       offset: 0
     },
     velocity: {
-      buffer: boidBuffer,
+      buffer: BOID_BUFFER,
       stride: BOID_DATA_SIZE,
       offset: 12
     },
     color: {
-      buffer: boidBuffer,
+      buffer: BOID_BUFFER,
       stride: BOID_DATA_SIZE,
       offset: 24
     }
   },
   uniforms: {
     view: ({ tick }) => {
-      const t = 0.025 * tick
+      const t = 0.01 * tick
       return mat4.lookAt([],
         [100 * Math.cos(t), 2.5, 100 * Math.sin(t)],
         [0, 0, 0],
@@ -168,19 +191,19 @@ const drawBoids = regl({
         1000)
     }
   },
-  count: NUM_BOIDS,
+  count: regl.prop('numBoids'),
   primitive: 'points'
 })
 
 let lastTime = 0
-regl.frame(({ time }) => {
+regl.frame(() => {
+  stats.begin()
   regl.clear({
     depth: 1,
     color: [0, 0, 0, 1]
   })
 
-  const timeStep = time - lastTime
-  lastTime = time
-  updateBoids()
-  drawBoids()
+  updateBoids(options)
+  drawBoids(options)
+  stats.end()
 })
